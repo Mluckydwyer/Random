48bd83540e07b7cf1c1dc9663ef650ebf63d969e
Matt Dwyer <763431@10.9.30.172>
2015-12-03 20:24:42 -0600

    Work a bit on prime numbers

--- Random/src/com/Prime/Prime.java
+++ Random/src/com/Prime/Prime.java
@@ -8,16 +8,17 @@ public class Prime {
 	public static String threadManagement;
 	public static char testChar;
 	public static int threadCount;
-	public static int startPoint = 1;//BigInteger startPoint = new BigInteger((Math.pow(2, 57885161) + 1));
+	public static int startPoint = 1;// BigInteger startPoint = new
+										// BigInteger((Math.pow(2, 57885161) +
+										// 1));
 	public static boolean PRIME;
-	public static double increment = 2000000;//2,000,000
+	public static double increment = 2000000;// 2,000,000
 	public static double ThreadStartPoint;
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 
 		while (threadManagement == null) {
-			System.out.println(Math.pow(2, 57885161) + 1);
 			System.out.println("What type of thread management would you like? (Automatic-Number of CPU Cores, Manual-User Specifyed)");
 			String s = input.nextLine();
 			Scanner findChar = new Scanner(s);
@@ -28,11 +29,13 @@ public class Prime {
 				threadManagement = Character.toString(testChar);
 				threadCount = Runtime.getRuntime().availableProcessors();
 				System.out.println("System detected " + threadCount + " availble physical cores");
-			} else if (Character.toString((testChar)).matches("m") || Character.toString((testChar)).matches("M")) {
+			}
+			else if (Character.toString((testChar)).matches("m") || Character.toString((testChar)).matches("M")) {
 				threadManagement = Character.toString(testChar);
 				System.out.println("How many threads would you like?");
 				threadCount = input.nextInt();
-			} else {
+			}
+			else {
 				System.out.println("That is not a valid option");
 			}
 		}
@@ -40,13 +43,14 @@ public class Prime {
 		System.out.println("Spawing Threads...");
 		ArrayList<Thread> threads = new ArrayList<Thread>();
 
-		for (int x = 0; x == threadCount; x++) {
-			//ThreadStartPoint = startPoint = (x * increment);
-			
+		for (int x = 0; x < threadCount; x++) {
+			// ThreadStartPoint = startPoint = (x * increment);
+
 			if (x == threadCount) {
 				threads.add(new Thread(new PrimeThread(startPoint, increment, true)));
 				threads.get(threads.size() - 1).start();
-			} else {
+			}
+			else {
 				threads.add(new Thread(new PrimeThread(startPoint, increment, false)));
 				threads.get(threads.size() - 1).start();
 			}
--- Random/src/com/Prime/PrimeThread.java
+++ Random/src/com/Prime/PrimeThread.java
@@ -3,13 +3,12 @@ package com.Prime;
 public class PrimeThread implements Runnable {
 
 	double startNum;
-	double increment;
+	double stopNum;
 	boolean PRIME;
 	boolean PRIME_THREAD;
 
 	public PrimeThread(double startNum, double increment, boolean PRIME) {
 		this.startNum = startNum;
-		this.increment = increment;
 		this.PRIME_THREAD = PRIME;
 	}
 
@@ -17,7 +16,7 @@ public class PrimeThread implements Runnable {
 
 		System.out.println("These numbers are prime:");
 		System.out.println("Starting at " + startNum);
-		for (double y = startNum; y <= (startNum + increment); y += 2) {
+		for (double y = startNum; y <= stopNum; y += 2) {
 			PRIME = true;
 			for (int x = 3; x <= (y / 2); x += 2) {
 				if (y % x == 0) {
@@ -28,17 +27,11 @@ public class PrimeThread implements Runnable {
 			if (PRIME) {
 				System.out.println(y + " is prime");
 			}
-			
+
 			if (PRIME_THREAD) {
 				System.out.println(y + "/" + (startNum + increment));
 			}
-			
-			try {
-				Thread.sleep(250);
-			}
-			catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+
 		}
 	}
 }
